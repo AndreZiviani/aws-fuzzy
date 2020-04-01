d68aab7b77bb02f70feba07cfee99d3c40fca8cb
Andre Ziviani <andrepziviani@gmail.com>
2020-04-01 13:59:19 -0300

    feat/option: add cache option

--- aws_fuzzy/commands/common.py
+++ aws_fuzzy/commands/common.py
@@ -2,7 +2,12 @@ import click
 import functools
 
 
-def common_params(a="all", r="all", f="''", p=True, l=0):
+AWS_DIR = expanduser("~") + "/.aws"
+SSO_CRED_DIR = AWS_DIR + "/sso/cache"
+SSO_PROFILES = AWS_DIR + "/config"
+
+
+def common_params(a="all", r="all", f="''", p=True, c=True, l=0):
     def params(func):
         @click.option(
             '-a',
@@ -30,11 +35,41 @@ def common_params(a="all", r="all", f="''", p=True, l=0):
             show_default=True,
             help='Send query results to pager')
         @click.option(
+            '--cache/--no-cache',
+            'cache',
+            flag_value=True,
+            default=c,
+            show_default=True,
+            help='Whether to use cached results')
+        @click.option(
             '-l',
             '--limit',
             default=l,
             show_default=True,
             help='Use a custom query to filter results')
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return params
+
+
+def cache_params(cache=True, cache_time=3600):
+    def params(func):
+        @click.option(
+            '--cache/--no-cache',
+            'cache',
+            flag_value=True,
+            default=cache,
+            show_default=True,
+            help='Whether to use cached results')
+        @click.option(
+            '--cache-time',
+            default=cache_time,
+            show_default=True,
+            help='Cache results TTL in seconds')
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
