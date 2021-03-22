79761767b68cf1e409372ee1d04a5dae2f249ebe
Andre Ziviani <andrepziviani@gmail.com>
2021-03-22 16:45:52 -0300

    feat/goroutines: implement goroutines to paralelize transit gateway routes discovery

--- internal/chart/tgroutes.go
+++ internal/chart/tgroutes.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 
 	"github.com/AndreZiviani/aws-fuzzy/internal/common"
 	"github.com/AndreZiviani/aws-fuzzy/internal/sso"
@@ -12,10 +13,94 @@ import (
 	"github.com/AndreZiviani/aws-fuzzy/internal/vpc"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
 	opentracing "github.com/opentracing/opentracing-go"
 )
+
+func waitTGChannel(c chan<- []ec2types.TransitGatewayAttachment, wg *sync.WaitGroup) {
+	wg.Wait()
+	close(c)
+}
+
+func getTGAttachmentsRoutine(ctx context.Context, ec2client *ec2.Client, attachmentId string, c chan<- []ec2types.TransitGatewayAttachment, wg *sync.WaitGroup) {
+	defer wg.Done()
+	attachments, _ := vpc.GetTransitGatewayAttachmentsByAttachment(ctx, ec2client, attachmentId)
+	c <- attachments
+}
+
+func getSubnetFromTable(attachments []ec2types.TransitGatewayAttachment, table *vpc.DescribeTransitGatewayRouteTablesOutput) string {
+	// find destination cidr block
+	for _, route := range table.Routes {
+		if aws.ToString(route.TransitGatewayAttachments[0].TransitGatewayAttachmentId) == aws.ToString(attachments[0].TransitGatewayAttachmentId) {
+			return aws.ToString(route.DestinationCidrBlock)
+		}
+
+	}
+	return ""
+}
+
+func processTables(ctx context.Context, ec2client *ec2.Client, tables []*vpc.DescribeTransitGatewayRouteTablesOutput) ([]*opts.TreeData, error) {
+	tableNode := make([]*opts.TreeData, 0)
+
+	for _, table := range tables {
+		ipRanges := make([]*opts.TreeData, 0)
+
+		c := make(chan []ec2types.TransitGatewayAttachment)
+		var wg sync.WaitGroup
+
+		for _, ipRange := range table.Routes {
+			wg.Add(1)
+
+			//TODO: iterate attachments
+			attachmentId := aws.ToString(ipRange.TransitGatewayAttachments[0].TransitGatewayAttachmentId)
+
+			// Describe attachments in parallel
+			go getTGAttachmentsRoutine(ctx, ec2client, attachmentId, c, &wg)
+		}
+
+		// Wait for all routines to finish, then close channel to free main thread
+		go waitTGChannel(c, &wg)
+
+		for attachments := range c {
+
+			subnet := getSubnetFromTable(attachments, table)
+
+			//TODO: iterate attachments
+			account, _, err := sso.GetAccount(aws.ToString(attachments[0].ResourceOwnerId))
+			if err != nil {
+				account = attachments[0].ResourceOwnerId
+			}
+
+			name := fmt.Sprintf("%s\n%s\n%s",
+				common.GetEC2Tag(attachments[0].Tags, "Name", aws.ToString(attachments[0].TransitGatewayAttachmentId)),
+				aws.ToString(account),
+				aws.ToString(attachments[0].ResourceId),
+			)
+
+			destination := []*opts.TreeData{
+				{
+					Name: name,
+				},
+			}
+
+			ipNode := &opts.TreeData{
+				Name:     subnet,
+				Children: destination,
+			}
+
+			ipRanges = append(ipRanges, ipNode)
+		}
+		tmp := &opts.TreeData{
+			Name:     table.Name,
+			Children: ipRanges,
+		}
+		tableNode = append(tableNode, tmp)
+	}
+
+	return tableNode, nil
+}
 
 func (p *TGroutesCommand) Execute(args []string) error {
 	ctx := context.Background()
@@ -27,7 +112,7 @@ func (p *TGroutesCommand) Execute(args []string) error {
 	defer closer.Close()
 
 	tracer := opentracing.GlobalTracer()
-	span, ctx := opentracing.StartSpanFromContextWithTracer(ctx, tracer, "chart")
+	span, ctx := opentracing.StartSpanFromContextWithTracer(ctx, tracer, "tgroute chart")
 	defer span.Finish()
 
 	creds, err := sso.GetCredentials(ctx, p.Profile, false)
@@ -47,50 +132,7 @@ func (p *TGroutesCommand) Execute(args []string) error {
 
 	tables, _ := vpc.DescribeTransitGatewayRouteTables(ctx, ec2client)
 
-	tableNode := make([]*opts.TreeData, 0) // root children
-
-	//TODO: iterate tables
-	for _, table := range tables {
-		ipRanges := make([]*opts.TreeData, 0)
-
-		for _, ipRange := range table.Routes {
-			subnet := aws.ToString(ipRange.DestinationCidrBlock)
-			//TODO: iterate attachments
-			attachmentId := aws.ToString(ipRange.TransitGatewayAttachments[0].TransitGatewayAttachmentId)
-
-			attachments, _ := vpc.GetTransitGatewayAttachmentsByAttachment(ctx, ec2client, attachmentId)
-
-			//TODO: iterate attachments
-			account, _, err := sso.GetAccount(aws.ToString(attachments[0].ResourceOwnerId))
-			if err != nil {
-				account = attachments[0].ResourceOwnerId
-			}
-
-			name := fmt.Sprintf("%s\n%s\n%s",
-				common.GetEC2Tag(attachments[0].Tags, "Name", aws.ToString(attachments[0].TransitGatewayAttachmentId)),
-				aws.ToString(account),
-				aws.ToString(attachments[0].ResourceId),
-			)
-
-			destination := []*opts.TreeData{
-				&opts.TreeData{
-					Name: name,
-				},
-			}
-
-			ipNode := &opts.TreeData{
-				Name:     subnet,
-				Children: destination,
-			}
-
-			ipRanges = append(ipRanges, ipNode)
-		}
-		tmp := &opts.TreeData{
-			Name:     table.Name,
-			Children: ipRanges,
-		}
-		tableNode = append(tableNode, tmp)
-	}
+	tableNode, _ := processTables(ctx, ec2client, tables) // root children
 
 	rootNode := []opts.TreeData{
 		opts.TreeData{
