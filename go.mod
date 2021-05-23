863879447a6e41bb17953e3ca3ccf9ed0f40a19d
Andre Ziviani <7469258+AndreZiviani@users.noreply.github.com>
2021-05-23 14:27:32 -0300

    feat/tui: refact tui to use tview and custom fzf lib instead of ktr0731/go-fuzzyfinder (#31)
    * chore/cleanup: update dependencies
    
    * feat/tui: refact tui to use tview and custom fzf lib instead of ktr0731/go-fuzzyfinder
    
    * chore/debug: remove debug
    
    * fix/crash: fix crash when user abort selection
    
    * skip building windows arm{,64} and darwin arm
    
    * chore/tui: split tui config from data logic
    
    * chore/modules: update modules

--- go.mod
+++ go.mod
@@ -3,29 +3,40 @@ module github.com/AndreZiviani/aws-fuzzy
 go 1.14
 
 require (
+	github.com/AndreZiviani/fzf-wrapper/v2 v2.0.0-20210523171949-c28ead4a0f86
 	github.com/HdrHistogram/hdrhistogram-go v1.0.1 // indirect
-	github.com/aws/aws-sdk-go-v2 v1.2.0
-	github.com/aws/aws-sdk-go-v2/config v1.1.1
-	github.com/aws/aws-sdk-go-v2/credentials v1.1.1
-	github.com/aws/aws-sdk-go-v2/service/configservice v1.1.1
-	github.com/aws/aws-sdk-go-v2/service/ec2 v1.1.1
-	github.com/aws/aws-sdk-go-v2/service/networkmanager v1.1.1
-	github.com/aws/aws-sdk-go-v2/service/sso v1.1.1
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.1.1
+	github.com/aws/aws-sdk-go-v2 v1.6.0
+	github.com/aws/aws-sdk-go-v2/config v1.3.0
+	github.com/aws/aws-sdk-go-v2/credentials v1.2.1
+	github.com/aws/aws-sdk-go-v2/service/configservice v1.5.1
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.7.1
+	github.com/aws/aws-sdk-go-v2/service/networkmanager v1.2.1
+	github.com/aws/aws-sdk-go-v2/service/sso v1.2.1
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.2.1
 	github.com/faabiosr/cachego v0.16.1
-	github.com/go-echarts/go-echarts/v2 v2.2.3
-	github.com/jessevdk/go-flags v1.4.0
-	github.com/ktr0731/go-fuzzyfinder v0.3.2
-	github.com/mattn/go-sqlite3 v1.14.6
-	github.com/nsf/termbox-go v0.0.0-20210114135735-d04385b850e8 // indirect
+	github.com/gdamore/tcell/v2 v2.3.3
+	github.com/go-echarts/go-echarts/v2 v2.2.4
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/junegunn/fzf v0.0.0-20210523023155-7e5aa1e2a5d7 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7
 	github.com/opentracing/opentracing-go v1.2.0
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/uber/jaeger-client-go v2.25.0+incompatible
-	github.com/uber/jaeger-lib v2.4.0+incompatible
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/tview v0.0.0-20210521091241-1fd4a5b7aab3
+	github.com/smartystreets/assertions v1.0.0 // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/uber/jaeger-client-go v2.29.0+incompatible
+	github.com/uber/jaeger-lib v2.4.1+incompatible
 	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
 	gopkg.in/ini.v1 v1.62.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
 )
 
 //replace github.com/go-echarts/go-echarts/v2 => ../go-echarts
 
 replace github.com/go-echarts/go-echarts/v2 => github.com/AndreZiviani/go-echarts/v2 v2.2.17
+
+//replace github.com/AndreZiviani/fzf-wrapper/v2 => /home/andre/git/fzf-wrapper
---
+++ internal/ssh/tui.go
@@ -0,0 +1,246 @@
+package ssh
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/AndreZiviani/aws-fuzzy/internal/common"
+	"github.com/AndreZiviani/fzf-wrapper/v2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/rivo/tview"
+)
+
+type Instance struct {
+	ec2types.Instance
+}
+
+func (i Instance) PrintName() string {
+	return fmt.Sprintf("%s (%s)", common.GetEC2Tag(i.Tags, "Name", "<missing name>"), aws.ToString(i.PrivateIpAddress))
+}
+
+func (i Instance) PrintDetails() string {
+	output := bytes.NewBufferString("")
+
+	fmt.Fprintf(output, "Name: %s\n", common.GetEC2Tag(i.Tags, "Name", "<missing name>"))
+
+	fmt.Fprintf(output, "InstanceId: %s\n", *i.InstanceId)
+
+	fmt.Fprintf(output, "PrivateIp: %s\n", *i.PrivateIpAddress)
+
+	fmt.Fprintf(output, "Subnet: %s\n", *i.SubnetId)
+
+	fmt.Fprintf(output, "InstanceType: %s\n", i.InstanceType)
+
+	key := "<unknown>"
+	if i.KeyName != nil {
+		key = *i.KeyName
+	}
+	fmt.Fprintf(output, "Key: %s\n", key)
+
+	lifecycle := "OnDemand"
+	if i.InstanceLifecycle != "" {
+		lifecycle = string(i.InstanceLifecycle)
+	}
+	fmt.Fprintf(output, "Lifecycle: %s\n", lifecycle)
+
+	fmt.Fprintf(output, "Tags:\n")
+	for _, t := range i.Tags {
+		fmt.Fprintf(output, "  - %s: %s\n", *t.Key, *t.Value)
+	}
+
+	fmt.Fprintf(output, "AMI: %s\n", *i.ImageId)
+
+	fmt.Fprintf(output, "BlockDevices:\n")
+	for _, b := range i.BlockDeviceMappings {
+		if b.Ebs.VolumeId == nil {
+			continue
+		}
+		fmt.Fprintf(output, "  - Id: %s\n", *b.Ebs.VolumeId)
+	}
+
+	fmt.Fprintf(output, "Interfaces:\n")
+	for _, net := range i.NetworkInterfaces {
+		var interfaceid, ip, subnet, vpc string
+
+		if net.NetworkInterfaceId == nil {
+			interfaceid = "<unknown>"
+		} else {
+			interfaceid = *net.NetworkInterfaceId
+		}
+
+		if net.PrivateIpAddress == nil {
+			ip = "<unknown>"
+		} else {
+			ip = *net.PrivateIpAddress
+		}
+
+		if net.SubnetId == nil {
+			subnet = "<unknown>"
+		} else {
+			subnet = *net.SubnetId
+		}
+
+		if net.VpcId == nil {
+			vpc = "<classic>"
+		} else {
+			vpc = *net.VpcId
+		}
+
+		fmt.Fprintf(output, "  - Id: %s\n    IP: %s\n    Subnet: %s\n    Vpc: %s\n", interfaceid, ip, subnet, vpc)
+	}
+
+	fmt.Fprintf(output, "SGs:\n")
+	for _, s := range i.SecurityGroups {
+		var groupid, groupname string
+
+		if s.GroupId == nil {
+			groupid = "<unknown>"
+		} else {
+			groupid = *s.GroupId
+		}
+
+		if s.GroupName == nil {
+			groupname = "<unknown>"
+		} else {
+			groupname = *s.GroupName
+		}
+
+		fmt.Fprintf(output, "  - %s %s\n", groupid, groupname)
+	}
+
+	vpc := "<classic>"
+	if i.VpcId != nil {
+		vpc = *i.VpcId
+	}
+	fmt.Fprintf(output, "Vpc: %s\n", vpc)
+
+	return output.String()
+}
+
+type Tui struct {
+	app             *tview.Application
+	flex            *tview.Flex
+	input           *tview.InputField
+	resourceList    *tview.List
+	resourceDetails *tview.TextView
+	fzf             *fzfwrapper.Wrapper
+	instances       []Instance
+	selected        *Instance
+	instanceIdx     []int
+}
+
+type FzfData struct {
+	Instances []Instance
+}
+
+func NewFzfData(instancesOutput *ec2.DescribeInstancesOutput) *FzfData {
+	f := FzfData{}
+
+	f.Instances = make([]Instance, 0)
+
+	for _, r := range instancesOutput.Reservations {
+		for _, i := range r.Instances {
+			tmp := Instance{}
+			tmp.Instance = i // force ec2type.Instance to be my Instance type, see embedded struct doc
+			f.Instances = append(f.Instances, tmp)
+		}
+	}
+
+	return &f
+}
+
+func (f FzfData) FzfInputList() []string {
+	out := make([]string, 0, f.FzfInputLen())
+
+	for _, i := range f.Instances {
+		out = append(out, i.PrintDetails())
+	}
+
+	return out
+}
+
+func (f FzfData) FzfInputLen() int {
+	return len(f.Instances)
+}
+
+func (t *Tui) resourceListFunc(id int, text string, secondary string, shortcut rune) {
+	t.resourceDetails.SetText(
+		fmt.Sprintf("%s\n", secondary),
+	)
+}
+
+func (t *Tui) inputFunc(text string) {
+	if text == "" {
+		t.resourceList.Clear()
+		last := len(t.instances) - 1
+		for k, v := range t.instances {
+			t.instanceIdx[last-k] = k
+			t.resourceList.InsertItem(
+				-t.resourceList.GetItemCount()-1,
+				v.PrintName(),
+				v.PrintDetails(),
+				0, nil,
+			)
+		}
+		return
+	}
+
+	t.fzf.SetPattern(text)
+	results, _ := t.fzf.Fuzzy()
+
+	t.resourceList.Clear()
+	t.resourceDetails.Clear()
+
+	last := len(results) - 1
+	for k, v := range results {
+		t.instanceIdx[last-k] = int(v.Item.Index())
+		i := t.instances[v.Item.Index()]
+		t.resourceList.InsertItem(
+			-t.resourceList.GetItemCount()-1,
+			tview.TranslateANSI(
+				i.PrintName(),
+			),
+			tview.TranslateANSI(v.HighlightResult()),
+			0, nil,
+		)
+	}
+
+	t.resourceList.SetCurrentItem(-1)
+	t.resourceList.SetOffset(0, 0)
+	boldItem(t.resourceList, t.resourceList.GetCurrentItem())
+}
+
+func tui(instancesOutput *ec2.DescribeInstancesOutput) (*Instance, error) {
+
+	t := NewTui()
+
+	fzfInput := NewFzfData(instancesOutput)
+	t.fzf.SetInput(fzfInput)
+	t.instances = fzfInput.Instances
+	t.instanceIdx = make([]int, len(t.instances))
+
+	last := len(t.instances) - 1
+
+	for k, v := range t.instances {
+		t.instanceIdx[last-k] = k // reverse order since we are adding items to the beggining of the list
+		t.resourceList.InsertItem(
+			-t.resourceList.GetItemCount()-1,
+			v.PrintName(),
+			v.PrintDetails(),
+			0, nil,
+		)
+	}
+
+	if err := t.app.SetRoot(t.flex, true).SetFocus(t.flex).Run(); err != nil {
+		panic(err)
+	}
+
+	if t.selected == nil {
+		// user aborted the selection (ctrl+c?)
+		return nil, fmt.Errorf("Aborting by user request\n")
+	}
+
+	return t.selected, nil
+}
