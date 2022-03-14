5e6ae1ca1f80c34e9561a6128c819e7f3a1030ef
Andre Ziviani <7469258+AndreZiviani@users.noreply.github.com>
2022-03-14 13:49:45 -0300

    drop sqlite due to limitations with cross compiling without CGO (#35)

--- internal/cache/main.go
+++ internal/cache/main.go
@@ -1,12 +1,10 @@
 package cache
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/AndreZiviani/aws-fuzzy/internal/common"
 	"github.com/faabiosr/cachego"
-	"github.com/faabiosr/cachego/sqlite3"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/faabiosr/cachego/file"
 	"os"
 )
 
@@ -34,17 +32,7 @@ func New(service string) (cachego.Cache, error) {
 		}
 	}
 
-	db, err := sql.Open("sqlite3", fmt.Sprintf("%s/cache.sqlite", cacheDir))
-	if err != nil {
-		fmt.Printf("failed to open cache, %s\n", err)
-		return nil, err
-	}
-
-	cache, err := sqlite3.New(db, service)
-	if err != nil {
-		fmt.Printf("failed to create sqlite db, %s\n", err)
-		return cache, err
-	}
+	cache := file.New(cacheDir)
 
 	return cache, nil
 }
