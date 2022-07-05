57492d592bfc1353bd1decc10de86be72dcab184
Andre Ziviani <andrepziviani@gmail.com>
2022-07-05 20:39:07 -0300

    fix sso console logic

--- internal/sso/console.go
+++ internal/sso/console.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/AndreZiviani/aws-fuzzy/internal/tracing"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/common-fate/granted/pkg/browsers"
 	"github.com/common-fate/granted/pkg/config"
 	"github.com/common-fate/granted/pkg/debug"
@@ -31,26 +30,28 @@ func (p *Console) Execute(args []string) error {
 		debug.CliVerbosity = debug.VerbosityDebug
 	}
 
-	creds, err := login.GetCredentials(ctx)
-	if err != nil {
-		return err
-	}
-
-	p.OpenBrowser(ctx, creds)
+	p.OpenBrowser(ctx)
 
 	return err
 }
 
-func (p *Console) OpenBrowser(ctx context.Context, credentials *aws.Credentials) error {
+func (p *Console) OpenBrowser(ctx context.Context) error {
 	span, ctx := opentracing.StartSpanFromContext(ctx, "ssorolecreds")
 	defer span.Finish()
 
-	login := Login{}
+	login := Login{Profile: p.Profile}
 	login.LoadProfiles()
 	profile, err := login.GetProfile(p.Profile)
 	if err != nil {
 		return err
 	}
+
+	credentials, err := login.GetCredentials(ctx)
+	if err != nil {
+		return err
+	}
+
+	session := browsers.SessionFromCredentials(*credentials)
 
 	region := p.Region
 	if len(profile.AWSConfig.Region) > 0 {
@@ -58,7 +59,7 @@ func (p *Console) OpenBrowser(ctx context.Context, credentials *aws.Credentials)
 	}
 
 	browserOpts := browsers.BrowserOpts{Profile: profile.Name}
-	url, err := browsers.MakeUrl(browsers.SessionFromCredentials(*credentials), browserOpts, "", region)
+	url, err := browsers.MakeUrl(session, browserOpts, "", region)
 	if err != nil {
 		return err
 	}
@@ -77,5 +78,5 @@ func (p *Console) OpenBrowser(ctx context.Context, credentials *aws.Credentials)
 		return nil
 	}
 
-	return browsers.LaunchConsoleSession(browsers.SessionFromCredentials(*credentials), browserOpts, "", region)
+	return browsers.LaunchConsoleSession(session, browserOpts, "", region)
 }
